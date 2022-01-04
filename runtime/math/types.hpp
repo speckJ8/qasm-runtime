4ee589a61e7462f467ea4dd17fc2e3b67ce010fc
Alcides Andrade <junioralcides1994@gmail.com>
2022-01-04 13:45:12 +0900

    math types

--- runtime/math/types.hpp
+++ runtime/math/types.hpp
@@ -8,11 +8,7 @@ namespace runtime {
 namespace math {
 
 using namespace std::complex_literals;
-using cx_t = std::complex<double>;
-using unitary2d_t = cx_t[4];
-using unitary4d_t = cx_t[16];
-using unitary_t = std::vector<cx_t>;
-using vector_t = std::vector<cx_t>;
+using cx_t = std::complex<float>;
 
 }
 }
