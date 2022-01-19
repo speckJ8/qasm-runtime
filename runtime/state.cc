ee7214355db91c71436d0dca2cb1f131ea9e18ad
Alcides Andrade <andrade.alcides.junior@gmail.com>
2022-01-19 12:46:53 +0900

    initialize new registers to the |0> computational basis vector

--- runtime/state.cc
+++ runtime/state.cc
@@ -1,10 +1,13 @@
 #include "state.hpp"
+#include <cassert>
 #include <cmath>
 
 namespace runtime {
 
 void State::add_register(std::string name, size_t size) {
+    assert(size > 0);
     math::vector_t new_state_registers(std::exp2l(size));
+    new_state_registers[0] = 1.f;
     if (__builtin_expect(_empty, 0)) {
         _state = std::move(new_state_registers);
     } else {
